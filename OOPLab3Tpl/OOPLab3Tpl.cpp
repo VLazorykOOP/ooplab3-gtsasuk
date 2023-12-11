1ca3a3103939d33bdc528a297025519831124926
gtsasuk <tsasiuk.yevhenii.clg@chnu.edu.ua>
2023-12-11 21:39:00 +0200

    test

--- OOPLab3Tpl/OOPLab3Tpl.cpp
+++ OOPLab3Tpl/OOPLab3Tpl.cpp
@@ -1,24 +1,15 @@
-﻿// OOPLab3Tpl.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №3. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
+﻿#include <iostream>
+#include "task1.hpp"
 
-#include <iostream>
-
-// Ваші файли загловки 
-//
-#include "Lab3Example.h"
 int main()
 {
-    std::cout << " Lab #3  !\n";
-//  Код виконання завдань
-//  Головне меню завдань
-//  Функції та класи можуть знаходитись в інших файлах проекту
+    int task = 1;
+    switch (task) {
+    case 1:
+        task1();
+        break;
+    }
 
-    int conv = 1;
-    if (conv == 1) conv = mainExample1();
-    if (conv == 1) conv = mainExample3(); 
-    if (conv == 1) conv = mainExample4();
 
 }
 
